15e9cfc690ff0910903ea1ebc82e0cf8e6ac3db0
Miguel Negrón <manegron@redborder.com>
2025-05-20 12:53:56 +0100

    Add rb_persec.sh

---
+++ resources/bin/rb_persec.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#######################################################################
+# Copyright (c) 2014 ENEO Tecnología S.L.
+# This file is part of redBorder.
+# redBorder is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# redBorder is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License License for more details.
+# You should have received a copy of the GNU Affero General Public License License
+# along with redBorder. If not, see <http://www.gnu.org/licenses/>.
+#######################################################################
+
+[ $# -ge 1 -a -f "$1" ] && input="$1" || input="-"
+
+#cat $input | perl -ne 'print time(), "\n";' | uniq -c
+cat $input | pv -i2 -ltr > /dev/null
+
